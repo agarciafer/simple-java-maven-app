f4951255dda3d774bb9d871373477e131049c3cd
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2023-02-03 13:18:17 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World-version-6!";
+    private final String message = "Hello World-version-7-03/02/2023!";
 
     public App() {}
 
