bae8f0fe7b7ff98a7d71163424e9bcbe6d672cb8
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2024-06-11 12:39:03 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World-v8!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World-v8!\"");
+            fail("\"message\" is not \"Hello World-v9!\"");
         }
     }
 
