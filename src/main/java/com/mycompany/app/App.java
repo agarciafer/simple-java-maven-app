cfd2f68bfa1ee618b6291a1c10cc4d17c048baed
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2025-06-12 09:59:06 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World-v14-Lunes26!";
+    private final String message = "Hello World-v1-Jueves12!";
 
     public App() {}
 
