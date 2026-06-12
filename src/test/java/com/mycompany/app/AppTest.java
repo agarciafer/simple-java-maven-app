742da3282399f8812b0668a23213531f697f8c69
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2026-06-12 19:12:51 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World-v4-Viernes12!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World-v44-Viernes12!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World-v4-Viernes12!\"");
+            fail("\"message\" is not \"Hello World-v44-Viernes12!\"");
         }
     }
 
