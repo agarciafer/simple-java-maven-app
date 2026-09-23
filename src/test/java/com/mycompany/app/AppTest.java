7b43cc05c9e10b42afa27e9e0df203af9153a051
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2026-09-23 17:31:30 +0200

    Update expected output in AppTest

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World-v6!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World-v6borrar!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World-v6!\"");
+            fail("\"message\" is not \"Hello World-v6borrar!\"");
         }
     }
 
